4fe2005089e7e4937ab4e2173888d13366000af4
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2020-10-02 19:13:42 -0700

    Intial version with the Twilio API

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const twilio = require('twilio');
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const urlencoded = require('body-parser').urlencoded;
+
+const app = express();
+const port = 3000;
+
+// Parse incoming POST params with Express middleware
+app.use(urlencoded({ extended: false }));
+
+
+// Create a route that will handle Twilio webhook requests, sent as an
+// HTTP POST to /voice in our application
+app.post('/voice', (request, response) => {
+  // Use the Twilio Node.js SDK to build an XML response
+  const twiml = new VoiceResponse();
+
+  /** helper function to set up a <Gather> */
+  function gather() {
+    const gatherNode = twiml.gather({ numDigits: 1 });
+    gatherNode.say('For sales, press 1. For support, press 2.');
+
+    // If the user doesn't enter input, loop
+    twiml.redirect('/voice');
+  }
+
+  // If the user entered digits, process their request
+  if (request.body.Digits) {
+    switch (request.body.Digits) {
+      case '1':
+        twiml.say('You selected sales. Good for you!');
+        break;
+      case '2':
+        twiml.say('You need support. We will help!');
+        break;
+      default:
+        twiml.say("Sorry, I don't understand that choice.")
+        twiml.pause();
+        gather();
+        break;
+    }
+  } else {
+    // If no input was sent, use the <Gather> verb to collect user input
+    gather();
+  }
+
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});

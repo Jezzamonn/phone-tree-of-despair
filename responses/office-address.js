05d820f7da5e38bc0a13e474affefce38a206a1e
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2020-10-04 09:26:27 -0700

    Add voice clips for the office address part!

--- responses/office-address.js
+++ responses/office-address.js
@@ -1,4 +1,4 @@
-const { hold } = require('./common');
+const { hold, playSound } = require('./common');
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
 /**
@@ -11,7 +11,8 @@ function officeAddress(twiml) {
         action: '/office-address2',
         timeout: 10,
     });
-    gatherNode.say(`Hi, you've reached the "what is the address of my registration office" department. Now, you're gonna have to be patient, 'cause we're having some database problems at the moment. Um... actually would you mind helping? I need you to look up the postal code of a few places. Um, lets start with Antelope Hills, Wyoming, USA. Can you key in the ZIP code?`);
+    // gatherNode.say(`Hi, you've reached the "what is the address of my registration office" department. Now, you're gonna have to be patient, 'cause we're having some database problems at the moment. Um... actually would you mind helping? I need you to look up the postal code of a few places. Um, lets start with Antelope Hills, Wyoming, USA. Can you key in the ZIP code?`);
+    playSound(gatherNode, 'office-address/office-address-intro.mp3');
 
     twiml.redirect('./office-address-too-long');
 }
@@ -21,7 +22,7 @@ function officeAddress(twiml) {
  * @param {Request} request
  */
 function officeAddress2(twiml, request) {
-    if (!request.body.Digits != '82601') {
+    if (request.body.Digits != '82601') {
         officeAddressWrong(twiml);
         return;
     }
@@ -32,7 +33,8 @@ function officeAddress2(twiml, request) {
         action: '/office-address3',
         timeout: 10,
     });
-    gatherNode.say(`Ok.... That looks right. Ok, now I need the postal code of... Kathu, South Africa.`);
+    // gatherNode.say(`Ok.... That looks right. Ok, now I need the postal code of... Kathu, South Africa.`);
+    playSound(gatherNode, 'office-address/office-address-kathu.mp3');
 
     twiml.redirect('./office-address-too-long');
 }
@@ -42,7 +44,7 @@ function officeAddress2(twiml, request) {
  * @param {Request} request
  */
 function officeAddress3(twiml, request) {
-    if (!request.body.Digits != '8446') {
+    if (request.body.Digits != '8446') {
         officeAddressWrong(twiml);
         return;
     }
@@ -53,7 +55,8 @@ function officeAddress3(twiml, request) {
         action: '/office-address4',
         timeout: 10,
     });
-    gatherNode.say(`Ok... Great, that worked too. Um. Can you also check the postal code of White Cliffs, New South Wales, Australia?`);
+    // gatherNode.say(`Ok... Great, that worked too. Um. Can you also check the postal code of White Cliffs, New South Wales, Australia?`);
+    playSound(gatherNode, 'office-address/office-address-white-cliffs.mp3');
 
     twiml.redirect('./office-address-too-long');
 }
@@ -63,22 +66,25 @@ function officeAddress3(twiml, request) {
  * @param {Request} request
  */
 function officeAddress4(twiml, request) {
-    if (!request.body.Digits != '2836') {
+    if (request.body.Digits != '2836') {
         officeAddressWrong(twiml);
         return;
     }
 
-    twiml.say(`Ok.... Looks good. Ok, the system is loading. doot do do... Still loading... Ok, it's up! Let me see. Uh... Oh that's right, we only have one office. It's postal code is 5 6 7 8 9. That should be what you need. Oh and the system crashed again. Great. Well, yeah, that's all you need, 5 6 7 8 9. Ok... Bye.`);
+    // twiml.say(`Ok.... Looks good. Ok, the system is loading. doot do do... Still loading... Ok, it's up! Let me see. Uh... Oh that's right, we only have one office. It's postal code is 5 6 7 8 9. That should be what you need. Oh and the system crashed again. Great. Well, yeah, that's all you need, 5 6 7 8 9. Ok... Bye.`);
+    playSound(twiml, 'office-address/office-address-correct.mp3');
 }
 
 // Not exported
 function officeAddressWrong(twiml) {
-    twiml.say(`Ok... Er, looks like the system crashed. I guess that wasn't right. Let me- let me put you on hold and I'll try rebooting it.`);
+    // twiml.say(`Ok... Er, looks like the system crashed. I guess that wasn't right. Let me- let me put you on hold and I'll try rebooting it.`);
+    playSound(twiml, 'office-address/office-address-wrong.mp3');
     hold(twiml);
 }
 
 function officeAddressTooLong(twiml) {
-    twiml.say(`Oh sorry, the system crashed. Let me put you on hold for a second.`);
+    // twiml.say(`Oh sorry, the system crashed. Let me put you on hold for a second.`);
+    playSound(twiml, 'office-address/office-address-too-long.mp3');
     hold(twiml);
 }
 

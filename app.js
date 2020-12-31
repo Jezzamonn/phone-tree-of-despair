e3a7051c892b31af1fd78e3539235680a8fe13f9
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2020-12-30 18:17:50 -0800

    Split app and server
    This lets the app be tested without accidentally starting a local server (which might clash with a real local server that's running)

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const urlencoded = require('body-parser').urlencoded;
+const decamelize = require('decamelize');
+
+const { handleRequest } = require('./responses/common.js');
+const { generateToken } = require('./responses/token.js');
+
+const entryResponses = require('./responses/entry.js');
+const checkExtensionResponses = require('./responses/check-extension.js');
+const moreInfoResponses = require('./responses/more-info.js');
+const registrationResponses = require('./responses/registration.js');
+const directoryResponses = require('./responses/directory.js');
+const addressResponses = require('./responses/address.js');
+const officeAddressResponses = require('./responses/office-address.js');
+const datetimeResponses = require('./responses/datetime.js');
+const idResponses = require('./responses/id.js');
+const victoryResponses = require('./responses/victory.js');
+
+const responseGroups = [
+    entryResponses,
+    checkExtensionResponses,
+    moreInfoResponses,
+    registrationResponses,
+    directoryResponses,
+    addressResponses,
+    officeAddressResponses,
+    datetimeResponses,
+    idResponses,
+    victoryResponses,
+]
+
+const app = express();
+
+// Parse incoming POST params with Express middleware
+app.use(urlencoded({ extended: false }));
+
+// Create routes that will handle Twilio webhook requests, sent as
+// HTTP POST requests.
+console.log('Supported paths:')
+for (const responseGroup of responseGroups) {
+    for (const responseName of Object.keys(responseGroup)) {
+        const path = '/' + decamelize(responseName, '-');
+        app.post(path, handleRequest(responseGroup[responseName]));
+        console.log(`  ${path}`);
+    }
+}
+
+// Bonus API Key endpoint
+// TODO: CORS??
+console.log('  /token (bonus!)');
+app.post('/token', cors(), generateToken);
+
+module.exports = {
+    app // Visible for testing.
+};
--- server.js
+++ server.js
@@ -1,57 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const urlencoded = require('body-parser').urlencoded;
-const decamelize = require('decamelize');
+const { app } = require('./app.js');
 
-const { handleRequest } = require('./responses/common.js');
-const { generateToken } = require('./responses/token.js');
-
-const entryResponses = require('./responses/entry.js');
-const checkExtensionResponses = require('./responses/check-extension.js');
-const moreInfoResponses = require('./responses/more-info.js');
-const registrationResponses = require('./responses/registration.js');
-const directoryResponses = require('./responses/directory.js');
-const addressResponses = require('./responses/address.js');
-const officeAddressResponses = require('./responses/office-address.js');
-const datetimeResponses = require('./responses/datetime.js');
-const idResponses = require('./responses/id.js');
-const victoryResponses = require('./responses/victory.js');
-
-const responseGroups = [
-    entryResponses,
-    checkExtensionResponses,
-    moreInfoResponses,
-    registrationResponses,
-    directoryResponses,
-    addressResponses,
-    officeAddressResponses,
-    datetimeResponses,
-    idResponses,
-    victoryResponses,
-]
-
-const app = express();
 const port = process.env.PORT || 3000;
-
-// Parse incoming POST params with Express middleware
-app.use(urlencoded({ extended: false }));
-
-
-// Create routes that will handle Twilio webhook requests, sent as
-// HTTP POST requests.
-console.log('Supported paths:')
-for (const responseGroup of responseGroups) {
-    for (const responseName of Object.keys(responseGroup)) {
-        const path = '/' + decamelize(responseName, '-');
-        app.post(path, handleRequest(responseGroup[responseName]));
-        console.log(`  ${path}`);
-    }
-}
-
-// Bonus API Key endpoint
-// TODO: CORS??
-console.log('  /token (bonus!)');
-app.post('/token', cors(), generateToken);
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
